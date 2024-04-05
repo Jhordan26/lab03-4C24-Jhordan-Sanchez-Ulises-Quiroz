96a9e40636b8d94849d9715d2dc6e3aaf9d19de4
Daniq7059 <ulices.quiroz@tecsup.edu.pe>
2024-04-05 17:26:11 -0500

    Agergando cambios colaborador 2 - alumno

--- Colaborador.java
+++ Colaborador.java
@@ -2,3 +2,11 @@ public class Alumno{
     String nombre = "Ulices";
     String apellido = "Quiroz"
 }
+public class Nota extends Alumno{
+    Double nota1=20;
+    Double nota2=10;
+    Double nota3=15;
+    Double nota4=12.5;
+
+
+}
