b6668707f818bcf52e34a7cfc1cc9137141420a9
Jhordan Sanchez <jhordan.sanchez@tecsup.edu.pe>
2024-04-05 10:13:59 -0500

    Agregando Clase Docente

--- responsable.java
+++ responsable.java
@@ -2,6 +2,8 @@ public class Personas{
     String nombre = "Jhordan Sanchez";
     String apellido = "Sanchez Villegas"
     Int edad = 20;
-
-
+}
+public class Docente extends Personas{
+    Double sueldo = 200.50
+    
 }
